15e539affe844c1b1dde1fadfeeff12318a98140
Arjit <arjit25012@gmail.com>
2025-08-31 13:42:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+
+console.log(3
+    +
+    3
+)        //code readiablity should be high
+
+let name ="Arjit"
+let age = 18
+let isLoggedIn = false
+
+// number
+// bigint
+// boolean => true/false
+// undefined => 
+// null => standalone value
+// sring=""
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
