7e6e014eb6816c38b7593a4327e184e714a3bcba
Arjit <arjit25012@gmail.com>
2025-08-31 14:38:13 +0000

    typecoverstion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+/* let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+let isLoggedIn = "Arjit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+// "Arjit" = true
