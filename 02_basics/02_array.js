f2e15a978767a129ad87d9f92e89e92be49a3b2e
Arjit <arjit25012@gmail.com>
2025-09-05 17:24:43 +0000

    arrrays 2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,4 +1,18 @@
 const marvel_heros = ["thor","Ironman","spiderman"]
 const dc_heros = ["superman","flash","batman"]
 
- 
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+
+const all_new_heros = [...marvel_heros,...dc_heros]
+console.log(all_new_heros);
+
+
+
+ // read about Array.of ,Array.from,isArray
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+
+ console.log(Array.of(score1,score2,score3));
