3b057c811adcdcf8160713d93d4d4f952cff530e
Arjit <arjit25012@gmail.com>
2025-09-05 05:48:18 +0000

    date and time

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+
+
+let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myCreatedDate.getTime());
+console.log(Date.now());
+
