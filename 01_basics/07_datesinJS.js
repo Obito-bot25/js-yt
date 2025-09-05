c068d1fc259907613a5b3798d932f4b5c2ce7754
Arjit <arjit25012@gmail.com>
2025-09-05 14:08:47 +0000

    date and time

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -8,3 +8,15 @@ let myTimeStamp = Date.now()
 console.log(myCreatedDate.getTime());
 console.log(Date.now());
 
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDate());
+
+`${newDate.getDate()} and the time `
+
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+    
+
+})
