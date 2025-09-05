a464ebd3c83a9742c0a32850954407d4af7af557
Arjit <arjit25012@gmail.com>
2025-09-05 14:44:45 +0000

    Arrays 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,20 @@
+// Array
+
+const myArr = [0,1,2,3,4,true,"Arjit"]
+const myHeros = ["Ironman","Spiderman"]
+console.log(myArr)
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+console.log(myArr2[1]);
+
+// Array methods 
+
+
+console.log(myArr.includes(9));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+
