01f74f05e2a3bd58f354e1c808d70e0f442e4334
Arjit <arjit25012@gmail.com>
2025-09-06 05:59:43 +0000

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename : "Js in Hindi",
+    price: "999",
+    courseInstructor : "hitesh"
+
+}
+
+const  {courseInstructor } = course
+
+console.log(courseInstructor);
+
