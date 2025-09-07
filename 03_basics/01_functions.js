0f7eb655132fd25c9d9bb2f605efad0bc5cd4adb
Arjit <arjit25012@gmail.com>
2025-09-07 04:41:22 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,6 +23,29 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("hitesh"))
-console.log(loginUserMessage("hitesh"))
+//console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500))
+
+const user = {
+    username: "Arjit",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+//handleObject(user)
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    console.log(getArray[1])
+}
+returnSecondValue(myNewArray);
 
 
