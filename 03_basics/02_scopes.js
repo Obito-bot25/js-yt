11d3b1067bdb748d2d2ebc1a0945626a057d19f8
Arjit <arjit25012@gmail.com>
2025-09-20 11:50:02 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,53 @@
+let a = 300
+
+if(true){
+    let a = 10
+const b = 20
+
+    
+}
+
+//console.log(a);
+// console.log(b);
+//console.log(c);
+
+
+function one(){
+    const username = "Arjit"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+    //two()
+}
+
+ // one()
+
+ if(true){
+    const username = "Arjit"
+    if (username === "Arjit"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+
+    //console.log(website);
+
+ }
+ //console.log(username)
+
+ //++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++++++++//
+
+ function addone(value){
+    return num + 1
+ }
+
+ console.log(addone(5))
+
+ const addTwo = function(num){
+    return num + 2 
+ }
+
+addTwo(5)
